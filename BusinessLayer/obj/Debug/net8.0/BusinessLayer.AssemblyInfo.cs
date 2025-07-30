baa29c23fcbe367bbba631257de0c58c60456f67
SE184400-PhamTanLoc <locptse184400@fpt.edu.vn>
2025-07-30 20:39:19 +0700

    update

--- BusinessLayer/obj/Debug/net8.0/BusinessLayer.AssemblyInfo.cs
+++ BusinessLayer/obj/Debug/net8.0/BusinessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6337e35954cd2b23451984f3f17d814137d6afa7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98f49b3dbc75995415e78861b1d9437024a0eb10")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
